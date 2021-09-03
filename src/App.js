dbbf9e2a1e44181c0c53c8806265695d40dfff75
troy <hunter007nz@gmail.com>
2021-09-03 23:04:41 +1200

    added react router base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link, 
+  NavLink 
+} from "react-router-dom";
+
+
 function App() {
   return (
     <div>
+
+
+      <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Products">Products</NavLink>
+            </li>
+            <li>
+              <NavLink to="/users">Users</NavLink>
+            </li>
+          </ul>
+        </nav>
       <h2>Let's get started!</h2>
+      <Switch>
+        <Route path="/Products">
+          <Products />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+   
     </div>
+
+
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Products() {
+  return <h2>Products</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
